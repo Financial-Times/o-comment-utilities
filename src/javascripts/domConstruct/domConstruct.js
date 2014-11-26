44409bc391c3a946bfc6546c2ca80d1b10829446
Roland Vachter <roland.vachter@ft.com>
2014-11-26 17:08:48 +0200

    DOM construct with event instead of adding to the global scope.

--- src/javascripts/domConstruct/domConstruct.js
+++ src/javascripts/domConstruct/domConstruct.js
@@ -2,31 +2,37 @@
 
 var sizzle = require('sizzle');
 
-module.exports = function (Widget, baseClass) {
-    var instances = sizzle('.' + baseClass);
+module.exports = function (config) {
+    var instances = sizzle('.' + config.baseClass);
 
     var item;
     for (var i = 0; i < instances.length; i++) {
         item = instances[i];
 
-        if (item.getAttribute('data-' + baseClass + '-autoconstruct') === "true" && item.id && item.innerHTML === "") {
-            var config = {
+        if (item.getAttribute('data-' + config.baseClass + '-autoconstruct') === "true" && item.id && item.innerHTML === "") {
+            var widgetConfig = {
                 elId: item.id
             };
             var match;
 
             for (var j = 0; j < item.attributes.length; j++) {
-                match = item.attributes[j].name.match(new RegExp('data-' + baseClass + '-config-(.*)'));
+                match = item.attributes[j].name.match(new RegExp('data-' + config.baseClass + '-config-(.*)'));
                 if (match && match.length) {
-                    config[match[1]] = item.attributes[j].value;
+                    widgetConfig[match[1]] = item.attributes[j].value;
                 }
             }
 
-            var widget = new Widget(config);
-            widget.load();
+            var widget = new config.Widget(widgetConfig);
 
-            item.setAttribute('data-' + baseClass + '-widget-id', baseClass + '-widget-' + item.id);
-            window[baseClass + '-widget-' + item.id] = widget;
+            document.body.dispatchEvent(new CustomEvent(config.namespace + '.domConstruct', {
+            	detail: {
+            		id: item.id,
+            		instance: widget
+            	},
+            	bubble: true
+            }));
+
+            widget.load();
         }
     }
 };
