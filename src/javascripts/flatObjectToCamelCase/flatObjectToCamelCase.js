985653cc039c143355dae55963927a0c0973c772
Roland Vachter <roland.vachter@ft.com>
2015-04-24 15:52:12 +0300

    CAE-322: Fix the logic of flatObjectToCamelCase function

--- src/javascripts/flatObjectToCamelCase/flatObjectToCamelCase.js
+++ src/javascripts/flatObjectToCamelCase/flatObjectToCamelCase.js
@@ -3,12 +3,10 @@
 function transform (currentName, level, sourceObj, resultObj) {
 	for (var key in sourceObj) {
 		if (sourceObj.hasOwnProperty(key)) {
-			currentName += (level === 0 ? key : key.charAt(0).toUpperCase() + key.slice(1));
-
 			if (sourceObj[key] && typeof sourceObj[key] === 'object' && !(sourceObj[key] instanceof Array) && Object.keys(sourceObj).length) {
-				transform(currentName, level + 1, sourceObj[key], resultObj);
+				transform(currentName + (level === 0 ? key : key.charAt(0).toUpperCase() + key.slice(1)), level + 1, sourceObj[key], resultObj);
 			} else {
-				resultObj[currentName] = sourceObj[key];
+				resultObj[currentName + (level === 0 ? key : key.charAt(0).toUpperCase() + key.slice(1))] = sourceObj[key];
 			}
 		}
 	}
