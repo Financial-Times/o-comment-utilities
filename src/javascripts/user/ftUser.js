53bda62c8589f4df227ce021f5940b9233e18c92
Roland Vachter <roland.vachter@ft.com>
2015-10-30 16:59:59 +0200

    FTUser is possibly logged in if there's an FTSession. getUserId and getEmail functions removed as they are legacy and should not be used.

--- src/javascripts/user/ftUser.js
+++ src/javascripts/user/ftUser.js
@@ -1,52 +1,11 @@
 const cookie = require('../cookie/cookie.js');
 
 /**
- * There are some FT cookies which incorporates multiple key value pairs.
- * As separator they use ":".
- *
- * Example: key1=value1:key2=value2.
- *
- * This helps retrieving from such a cookie different values by keys.
- *
- * @param  {String} cookieName Name of the cookie
- * @param  {String} param      Parameter to search within the cookie's value.
- * @return {String} Value of the cookie parameter.
- */
-function getCookieParam (cookieName, param) {
-	let match;
-
-	if (cookie.get(cookieName)) {
-		match = cookie.get(cookieName).match(new RegExp('(^|[:])'+ param +'=([^:]*)'));
-		if (match && match.length && match[2]) {
-			return match[2];
-		}
-	}
-
-	return null;
-}
-
-/**
  * Check if the cookies which shows that a user is possibly logged in are set.
  * @return {Boolean} If the user is logged in or not
  */
 function isLoggedIn () {
-	return !!getCookieParam('FT_User', 'EMAIL') || !!getCookieParam('FT_Remember', 'EMAIL');
-}
-
-/**
- * Reads the user's email address from the cookies set.
- * @return {String} Email address
- */
-function getEmail () {
-	return getCookieParam('FT_User', 'EMAIL') || getCookieParam('FT_Remember', 'EMAIL') || null;
-}
-
-/**
- * Reads the user's eRights ID from the cookies set.
- * @return {String} User ID
- */
-function getUserId () {
-	return getCookieParam('FT_User', 'ERIGHTSID') || null;
+	return !!getSession();
 }
 
 /**
@@ -63,7 +22,5 @@ function getSession () {
  */
 module.exports = {
 	isLoggedIn: isLoggedIn,
-	getEmail: getEmail,
-	getUserId: getUserId,
 	getSession: getSession
 };
